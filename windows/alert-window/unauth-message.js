035163aa2ada7c2c2c0654df5964fd550b8f246c
achBou <bounane.ach@gmail.com>
2021-10-12 13:20:12 +0200

    adding adding close button logic

--- windows/alert-window/unauth-message.js
+++ windows/alert-window/unauth-message.js
@@ -1,2 +1,8 @@
-
+window.addEventListener('DOMContentLoaded', () => {
+    const closeBtn = document.getElementById('closeBtn');
+    console.log(closeBtn)
+    closeBtn.addEventListener('click', (e)=>{
+        console.log("close window");
+    })
+})
 
