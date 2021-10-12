a29e12276bb5aa10fa20a75de5683056d0d164b0
achBou <bounane.ach@gmail.com>
2021-10-12 11:28:34 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+
+    win.loadFile('index.html')
+}
+
+
+// loading the app when its ready
+app.whenReady().then(() => {
+    createWindow()
+
+    // for macOS ( doesnt quit when all windows are closed )
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+// generally apps quit when all windows are closed on Mac and linux
+// but not on macOS
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
