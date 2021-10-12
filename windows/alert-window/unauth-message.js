3fc85d41704538e2744643d76cf933479e087e8e
achBou <bounane.ach@gmail.com>
2021-10-12 12:19:23 +0200

    updating the architecture with adding windows folder

---
+++ windows/alert-window/unauth-message.js
@@ -0,0 +1,11 @@
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    const closeBtn = document.getElementById('closeBtn');
+    console.log(closeBtn)
+    closeBtn.addEventListener('click',async (e)=>{
+        console.log("hello");
+        await window.alertWindow.close()
+    })
+})
+
