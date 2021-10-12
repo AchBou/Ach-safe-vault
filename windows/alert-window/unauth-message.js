2d1ff7c90a4fdc93d78d989a8671eaf8d3a3962a
achBou <bounane.ach@gmail.com>
2021-10-12 13:17:04 +0200

    adding a picture and some style to the intruder alert

--- windows/alert-window/unauth-message.js
+++ windows/alert-window/unauth-message.js
@@ -1,11 +1,2 @@
 
 
-window.addEventListener('DOMContentLoaded', () => {
-    const closeBtn = document.getElementById('closeBtn');
-    console.log(closeBtn)
-    closeBtn.addEventListener('click',async (e)=>{
-        console.log("hello");
-        await window.alertWindow.close()
-    })
-})
-
